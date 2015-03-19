8d5ff06cc6bba437798b1b967a9df70f7c8b04ae
ankur singh <ankur13019@iiitd.ac.in>
2015-03-20 05:12:54 +0530

    init

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$(document).ready(function (){
+
+  // create a LatLng object containing the coordinate for the center of the map
+  var latlng = new google.maps.LatLng(28.544390, 77.272444);
+
+  // prepare the map properties
+  var options = {
+    zoom: 15,
+    center: latlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    navigationControl: true,
+    mapTypeControl: false,
+    scrollwheel: false,
+    disableDoubleClickZoom: true
+  };
+
+  // initialize the map object
+  var map = new google.maps.Map(document.getElementById('google_map'), options);
+
+  // add Marker
+  var marker1 = new google.maps.Marker({
+    position: latlng, map: map
+  });
+
+  // add listener for a click on the pin
+  google.maps.event.addListener(marker1, 'click', function() {
+    infowindow.open(map, marker1);
+  });
+
+  // add information window
+  var infowindow = new google.maps.InfoWindow({
+    content:  '<div class="info"><strong>IIIT-Delhi</strong><br><br>Address:<br>Okhla Industrial Estate, Phase 3, New Delhi</div>'
+  });  
+  infowindow.open(map, marker1);
+});
+

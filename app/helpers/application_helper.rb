5b9fe8eaa548a39fb2791037775779aa8a83e4cf
ankur singh <ankur13019@iiitd.ac.in>
2015-03-26 20:35:04 +0530

    added current sale helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
 	def current_sale
-
+		@current_sale = Sale.where("start < ? AND end > ?",Time.now,Time.now)
 	end
 	def current_sale?
-
+		current_sale.present?
 	end
 end
